c314cd52e0a433eb8b6558f86e96478b030d78cc
Mark Weiman <mark.weiman@markzz.com>
2021-04-16 23:22:30 -0400

    meson-make-symlink.sh: Make compatible with non-GNU ln
    The "-T" flag is something available with GNU ln, but on other
    implementations of ln, "-T" is not an option. So, to make this script
    more portable, a strings check of ln for "GNU coreutils" is done to
    determine if "-T" should be used.
    
    Signed-off-by: Mark Weiman <mark.weiman@markzz.com>

--- build-aux/meson-make-symlink.sh
+++ build-aux/meson-make-symlink.sh
@@ -4,9 +4,13 @@ set -eu
 # this is needed mostly because $DESTDIR is provided as a variable,
 # and we need to create the target directory...
 
+# test if ln is GNU or not
+LN_FLAG="-T"
+strings /bin/ln | grep -q 'GNU coreutils' || LN_FLAG=""
+
 mkdir -vp "$(dirname "${DESTDIR:-}$2")"
 if [ "$(dirname $1)" = . ]; then
-        ln -vfs -T "$1" "${DESTDIR:-}$2"
+        ln -vfs ${LN_FLAG} "$1" "${DESTDIR:-}$2"
 else
-        ln -vfs -T --relative "${DESTDIR:-}$1" "${DESTDIR:-}$2"
+        ln -vfs ${LN_FLAG} --relative "${DESTDIR:-}$1" "${DESTDIR:-}$2"
 fi
